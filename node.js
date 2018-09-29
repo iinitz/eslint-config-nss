443d2dcb8499e6cc3ac777f462eb1e9e7b85435c
Saksit Sawang <hanamiza555@gmail.com>
2018-09-30 06:46:12 +0700

    change file structure

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'extends': 'nss/lib/defaults',
+  'extends': 'nss',
   'env': {
     'node': true,
     'es6': true,
--- react.js
+++ react.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'extends': 'nss/lib/defaults',
+  'extends': 'nss',
   'parserOptions': {
     'ecmaFeatures': {
       'jsx': true,
