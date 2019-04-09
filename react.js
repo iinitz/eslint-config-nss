9db2993a47dd5fb071594d5e800fc8e8d077bfc6
Saksit Sawang <hanamiza555@gmail.com>
2019-04-09 18:43:17 +0700

    add eslint, babel-eslint to dependencies

--- react.js
+++ react.js
@@ -8,9 +8,12 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
+    'jest': true,
   },
   'rules': {
     'react/jsx-filename-extension': 0,
     'function-paren-newline': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'lines-between-class-members': 0,
   },
 }
