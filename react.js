016521c3d2a4982e7c76edf0b66706e250087a8a
Saksit Sawang <hanamiza555@gmail.com>
2020-08-18 03:53:30 +0700

    update react rules

--- react.js
+++ react.js
@@ -10,19 +10,21 @@ module.exports = {
     'es6': true,
     'jest': true,
   },
-  "plugins": [
-    "react-hooks"
+  'plugins': [
+    'react-hooks'
   ],
   'rules': {
-    'react/jsx-filename-extension': 0,
-    'function-paren-newline': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'lines-between-class-members': 0,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": [
-      "warn",
+    'function-paren-newline': 'off',
+    'lines-between-class-members': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-fragments': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
       {
-        "additionalHooks": "useRecoilCallback"
+        'additionalHooks': 'useRecoilCallback'
       }
     ]
   },
